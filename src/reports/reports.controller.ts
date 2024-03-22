61ef7b0ba4d47a78ce49b68397c2f6285d36ef7c
Salomão Gonçalves Calheiros <salomao.calheiros@gmail.com>
2024-03-22 15:09:14 -0400

    reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Get, Logger, Param } from '@nestjs/common';
+import { ValidacaoParametrosPipe } from 'src/common/pipes/validacao-parametros.pipe';
 import { ClientProxyRadioFrequency } from 'src/common/proxyrmq/client-proxy';
 
 @Controller('api/reports')
@@ -17,6 +18,19 @@ export class ReportsController {
     async getReports(){
         this.logger.log(`reports GET`);
         return this.dominioReports.send('get-reports', {}).toPromise();
+    }
+
+    @Get('/analytics/:type')
+    async getAnalyticsByType(
+        @Param('type',ValidacaoParametrosPipe) type: string) {
+        this.logger.log(`reports GET`, type);
+        return this.dominioReports.send('get-analytics-by-type', type).toPromise();
+    }
+
+    @Get('/sintetic/:type')
+    async getSinteticByType(){
+        this.logger.log(`reports GET`);
+        return this.dominioReports.send('get-sintetic-by-type', {}).toPromise();
     }
 
 }
