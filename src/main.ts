6ba8f8ae33f787af344faa137ad42bc0411803e9
Salomão Gonçalves Calheiros <salomao.calheiros@gmail.com>
2024-03-27 10:46:41 -0400

    https

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,20 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { timeoutInterceptor } from './common/interceptors/timeout.interceptor';
-import { ClientProxyRadioFrequency } from './common/proxyrmq/client-proxy';
+import * as fs from 'fs';
 
 
 async function bootstrap() {
 
   await tracer;
+
+  const httpsOptions = {
+    key: fs.readFileSync('./src/cert/key.pem'),
+    cert: fs.readFileSync('./src/cert/cert.pem'),
+  };
   
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{ httpsOptions },);
+  
   app.useGlobalInterceptors(new LoggingInterceptor(), new timeoutInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
 
